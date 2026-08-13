9ad65c2070de6259622bb2a175dbdbb65e7f2c58
steddiK <steddi.andria@gmail.com>
2026-08-13 23:34:12 +0300

    integration authentification

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.routes import (
     auth
 )
@@ -6,3 +8,11 @@ from app.api.routes import (
 app = FastAPI(title="Gestion de Stock API")
 
 app.include_router(auth.routeur, prefix="/auth", tags=["Authentification"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # l'origine de ton frontend (Next.js en dev)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
